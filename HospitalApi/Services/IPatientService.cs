9175d5d95b8d57aad4253bc1e43b5f8e58546234
Altair_As <semalexandrov@gmail.com>
2024-09-18 18:05:14 +0300

    Add PatientService interface.

---
+++ HospitalApi/Services/IPatientService.cs
@@ -0,0 +1,13 @@
+﻿using HospitalApi.DTOs;
+
+namespace HospitalApi.Services
+{
+    public interface IPatientService
+    {
+        Task<IEnumerable<PatientDto>> GetPatientsAsync();
+        Task<PatientEditDto> GetPatientByIdAsync(int id);
+        Task AddPatientAsync(PatientEditDto patient);
+        Task UpdatePatientAsync(PatientEditDto patient);
+        Task DeletePatientAsync(int id);
+    }
+}
