1f7973ee35cd93928383a57cffc8f94b58fbbb51
Altair_As <semalexandrov@gmail.com>
2024-09-18 18:32:11 +0300

    Set up auto mapper.

---
+++ HospitalApi/Mappings/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HospitalApi.DTOs;
+using HospitalApi.Entities;
+
+namespace HospitalApi.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Patient, PatientDto>()
+                .ForMember(dest => dest.DistrictName, opt => opt.MapFrom(src => src.District.Number));
+
+            CreateMap<Patient, PatientEditDto>().ReverseMap();
+        }
+    }
+}
