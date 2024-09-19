e77a9d677d5bf75102a9341036c37b45a4dc83df
Altair_As <semalexandrov@gmail.com>
2024-09-19 19:08:33 +0300

    Add doctor repository.

---
+++ HospitalApi/Repositories/DoctorRepository.cs
@@ -0,0 +1,43 @@
+﻿using HospitalApi.Data;
+using HospitalApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalApi.Repositories
+{
+    public class DoctorRepository(AppDbContext context) : IDoctorRepository
+    {
+        public async Task<IEnumerable<Doctor>> GetAllAsync()
+        {
+            return await context.Doctors
+                .Include(d => d.Room)
+                .Include(d => d.District)
+                .Include(d => d.Specialization)
+                .ToListAsync();
+        }
+
+        public async Task<Doctor> GetByIdAsync(int id)
+        {
+            return await context.Doctors.FindAsync(id) 
+                ?? throw new NullReferenceException("Doctor not found");
+        }
+
+        public async Task AddAsync(Doctor doctor)
+        {
+            await context.AddAsync(doctor);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Doctor doctor)
+        {
+            context.Update(doctor);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var patient = await GetByIdAsync(id);
+            context.Remove(patient);
+            await context.SaveChangesAsync();
+        }
+    }
+}
